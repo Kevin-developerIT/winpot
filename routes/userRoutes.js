f5fb0cce1eb64e5675f91efd9ffa359a2d5d09be
MaguKill <94996470+MaguKill@users.noreply.github.com>
2024-11-16 10:52:45 -0600

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,18 +5,18 @@ const db = require('../db'); // Asegúrate de que db.js esté correctamente conf
 
 // Ruta para registrar un nuevo usuario
 router.post('/register', (req, res) => {
-    const { nombre, apellido, correo } = req.body;
+    const { first_name, last_name, team, age, email, phone } = req.body;
 
     // Validar que todos los campos estén presentes
-    if (!nombre || !apellido || !correo) {
+    if (!first_name || !last_name || !team || !age || !email || !phone ) {
         return res.status(400).json({ error: 'Por favor, completa todos los campos.' });
     }
 
     // Consulta para insertar un nuevo usuario en la base de datos
-    const sql = 'INSERT INTO users (nombre, apellido, correo) VALUES (?, ?, ?)';
+    const sql = 'INSERT INTO users (first_name, last_name, team, age, email, phone) VALUES (?, ?, ?)';
 
     // Ejecutar la consulta con los valores del formulario
-    db.query(sql, [nombre, apellido, correo], (err, result) => {
+    db.query(sql, [first_name, last_name, team, age, email, phone], (err, result) => {
         if (err) {
             console.error('Error al registrar el usuario:', err);
             return res.status(500).json({ error: 'Error al registrar el usuario' });
